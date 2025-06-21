da7a0864c6e04d41d0e6f869e6142fab4aa47b7a
oDyKoz <dymas10felipe20@gmail.com>
2025-06-20 22:23:19 -0300

    Mod-Display-Count-Burron

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import { ConfigModal } from "../functions/ModalConfig.js"; // Corrija o caminho se necessário
 import { limitarEntrada } from "../functions/limitarEntrada.js";
 import { PomoModal } from "../functions/ModalPomo.js";
+import { inject } from "@vercel/analytics"
 
 limitarEntrada();
 PomoModal();  
 ConfigModal();
+inject();
