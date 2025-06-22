594f68f1245e4b48bcafe42d0ace4f2cde7aeca7
oDyKoz <dymas10felipe20@gmail.com>
2025-06-22 19:50:58 -0300

    Mod-ModoDark

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import { ConfigModal } from "../functions/ModalConfig.js"; // Corrija o caminho se necessário
 import { limitarEntrada } from "../functions/limitarEntrada.js";
 import { PomoModal } from "../functions/ModalPomo.js";
-import { DarkModeConfig } from "../functions/DarkMode.js"
+import { DarkModeConfig } from "../functions/DarkMode.js";
 
 limitarEntrada();
 PomoModal();  
