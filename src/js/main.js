ea70a241a9c9420d04af4b55abeec96630918df1
oDyKoz <dymas10felipe20@gmail.com>
2025-06-22 19:45:50 -0300

    Mod-ModoDark

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import { ConfigModal } from "../functions/ModalConfig.js"; // Corrija o caminho se necessário
 import { limitarEntrada } from "../functions/limitarEntrada.js";
 import { PomoModal } from "../functions/ModalPomo.js";
-import { inject } from "@vercel/analytics"
+import { DarkModeConfig } from "../functions/DarkMode.js"
 
 limitarEntrada();
 PomoModal();  
 ConfigModal();
-inject();
+DarkModeConfig();
